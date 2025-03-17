3d31522dd8a7158b91ed9d5e9a8c818bba6c03dc
Lukas <lukasrs13@gmail.com>
2025-03-17 11:57:51 -0300

    update-17-03-25

---
+++ src/main/java/com/senac/apispring/aluno/Usuario.java
@@ -0,0 +1,9 @@
+package com.senac.apispring.aluno;
+
+public class Usuario {
+    private Long id;
+    private String nome;
+    private String email;
+    private String cpf;
+    private String password;
+}
